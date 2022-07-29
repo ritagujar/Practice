6da9ceee454bc0dff32ede9ba48027238bda648d
759153 <ritagujar14@gmail.com>
2022-07-29 12:16:02 +0530

    Added Animation on Navbar Links

--- html/drive-landing-page/app.js
+++ html/drive-landing-page/app.js
@@ -1,8 +1,21 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-menus");
+const navLinks = document.querySelectorAll(".nav-menus li");
+
 const navSide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-menus");
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        // console.log(index);
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
   });
 };
 
