327b343b7a4627259c0c666648d839bc9fe499f5
ritagujar <ritagujar14@gmail.com>
2024-03-09 13:47:22 +0530

    Added the Reset Functionality

--- JavaScript/Counter App/script.js
+++ JavaScript/Counter App/script.js
@@ -1,9 +1,14 @@
 const counter = document.querySelector(".counter");
 const increase = document.querySelector(".increase");
 const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
 
 increase.addEventListener("click", () => {
   counter.innerHTML++;
+});
+
+reset.addEventListener("click", () => {
+  counter.innerHTML = 0;
 });
 
 decrease.addEventListener("click", () => {
