562c8363d3b3bae0ca53174a95b83662539e2d39
ritagujar <ritagujar14@gmail.com>
2024-03-09 13:52:13 +0530

    Added the Functionality of color change of counter number

--- JavaScript/Counter App/script.js
+++ JavaScript/Counter App/script.js
@@ -5,12 +5,23 @@ const reset = document.querySelector(".reset");
 
 increase.addEventListener("click", () => {
   counter.innerHTML++;
+  counterColorChange();
 });
 
 reset.addEventListener("click", () => {
   counter.innerHTML = 0;
+  counterColorChange();
 });
 
 decrease.addEventListener("click", () => {
   counter.innerHTML--;
+  counterColorChange();
 });
+
+function counterColorChange() {
+  if (counter.innerHTML < 0) counter.style.color = "red";
+  else if (counter.innerHTML > 0) counter.style.color = "green";
+  else {
+    counter.style.color = "black";
+  }
+}
