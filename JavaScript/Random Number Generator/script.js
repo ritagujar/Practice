37ea3578a7ef25f98c50fdcbb0b15fd2cf5b88fe
ritagujar <ritagujar14@gmail.com>
2024-03-09 17:01:47 +0530

    Added comments

--- JavaScript/Random Number Generator/script.js
+++ JavaScript/Random Number Generator/script.js
@@ -4,7 +4,10 @@ const btn = document.querySelector(".btn");
 btn.addEventListener("click", generateRandomNumber);
 
 function generateRandomNumber() {
+  // Math.random() is an inbuilt method which returns a random value between 0 to 1 (for example 0.83746, 0.83847, 0.56233 ...)
+  // So multiplying by 100 will give us a value between 0 to 99.99999 and to include 100 we will need to add 1 in the end.
   const randomNumber = Math.floor(Math.random() * 100 + 1);
 
+  // Since Math.random gives us a lot of decimal numbers we will use Math.floor to remove the decimals and will get a whole number.
   number.innerHTML = randomNumber;
 }
