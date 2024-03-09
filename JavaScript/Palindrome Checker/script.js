acb0b8fee51919a61c337fbd64dadac94ab1c5ab
ritagujar <ritagujar14@gmail.com>
2024-03-09 23:17:50 +0530

    Added the alert

--- JavaScript/Palindrome Checker/script.js
+++ JavaScript/Palindrome Checker/script.js
@@ -4,19 +4,23 @@ const result = document.querySelector("#result");
 const checkPalindrome = () => {
   const word = document.querySelector("#input-check").value;
 
-  let length = word.length;
-
-  let start = word.substring(0, Math.floor(length / 2)).toLowerCase();
-
-  let end = word.substring(length - Math.floor(length / 2)).toLowerCase();
-
-  const reverse = [...end].reverse().join("");
-  console.log(reverse);
-
-  if (start == reverse) {
-    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+  if (word.length == 0) {
+    alert("Please Enter the Word");
   } else {
-    result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
+    let length = word.length;
+
+    let start = word.substring(0, Math.floor(length / 2)).toLowerCase();
+
+    let end = word.substring(length - Math.floor(length / 2)).toLowerCase();
+
+    const reverse = [...end].reverse().join("");
+    console.log(reverse);
+
+    if (start == reverse) {
+      result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+    } else {
+      result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
+    }
   }
 };
 
