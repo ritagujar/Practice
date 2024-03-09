4c4f8ea4a9cbdfd0f3011baa652104ce3e13600a
ritagujar <ritagujar14@gmail.com>
2024-03-09 23:14:21 +0530

    Added the palindrome check functionality

---
+++ JavaScript/Palindrome Checker/script.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+
+const checkPalindrome = () => {
+  const word = document.querySelector("#input-check").value;
+
+  let length = word.length;
+
+  let start = word.substring(0, Math.floor(length / 2)).toLowerCase();
+
+  let end = word.substring(length - Math.floor(length / 2)).toLowerCase();
+
+  const reverse = [...end].reverse().join("");
+  console.log(reverse);
+
+  if (start == reverse) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
+  }
+};
+
+btn.addEventListener("click", checkPalindrome);
