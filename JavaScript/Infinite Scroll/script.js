7c137887510f1c144873065baf8c5b90159170b8
ritagujar <ritagujar14@gmail.com>
2024-03-15 22:24:14 +0530

    Fetched the API Data

--- JavaScript/Infinite Scroll/script.js
+++ JavaScript/Infinite Scroll/script.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+const container = document.querySelector(".container");
+
+let limit = 4;
+let pageCount = 1;
+
+const getPosts = async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${pageCount}`
+  );
+  console.log(response);
+
+  const data = await response.json();
+  console.log(data);
+
+  data.map((curPost, index) => {
+    const postsHtmlData = `
+        <div class="posts">
+            <p class="posts_no">${curPost.id}</p>
+            <h2 class="posts_title">
+                ${curPost.title}
+            </h2>
+            <p class="posts_description">
+                ${curPost.body}
+            </p>
+        </div>
+        `;
+
+    container.insertAdjacentHTML("beforeend", postsHtmlData);
+  });
+};
+
+getPosts();
