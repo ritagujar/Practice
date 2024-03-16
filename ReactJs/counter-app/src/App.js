1ca43e034cf93bc8a1c450b77ada18a476f4e55c
ritagujar <ritagujar14@gmail.com>
2024-03-16 22:14:54 +0530

    Designed the app

--- ReactJs/counter-app/src/App.js
+++ ReactJs/counter-app/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Counter App</div>;
+  return (
+    <div className="app">
+      <h2 className="app-heading">Counter App</h2>
+      <span className="app-counter">0</span>
+      <div className="app-btn">
+        <button className="btn increase">Increase</button>
+        <button className="btn reset">Reset</button>
+        <button className="btn descrese">Decrese</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
