1a55cf7956bc2a39f02a5450f69a5d898b2e8382
ritagujar <ritagujar14@gmail.com>
2024-03-19 20:33:36 +0530

    Fetched and displayed the API data

--- JavaScript/Food App/script.js
+++ JavaScript/Food App/script.js
@@ -1,1 +1,30 @@
-console.log("Food App");
+const mealContainer = document.querySelector(".container__meals");
+
+const getAllFoodItems = async () => {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?f=s`
+  );
+  const data = await response.json();
+
+  console.log(data.meals);
+
+  data.meals.map((meal, index) => {
+    const postsHtmlData = `
+    <div class="container__meals--meal">
+        <div class="img__container">
+            <img src=${meal.strMealThumb} alt="pasta" class="container__meal--img" />
+        </div>
+         <div class="container__meals--content">
+            <h3 class="container__meal--title">${meal.strMeal}</h3>
+            <p class="container__meal--desc">
+               ${meal.strInstructions}
+            </p>
+        </div>
+    </div>
+    `;
+
+    mealContainer.insertAdjacentHTML("afterbegin", postsHtmlData);
+  });
+};
+
+getAllFoodItems();
