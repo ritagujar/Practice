ac55c4e480066372485ea9e85ea6f729a72a91d2
ritagujar <ritagujar14@gmail.com>
2024-03-22 21:49:03 +0530

    Completed the dark mode white mode project

--- JavaScript/Light-Dark Mode/script.js
+++ JavaScript/Light-Dark Mode/script.js
@@ -1,1 +1,25 @@
-console.log("Light and Dark mode");
+const toggleSwitch = document.querySelector("#toggle-switch");
+const box = document.querySelector(".box");
+const switchContainer = document.querySelector(".switch__container");
+
+const darkMode = () => {
+  box.style.color = "white";
+  switchContainer.style.color = "white";
+};
+
+const whiteMode = () => {
+  box.style.color = "black";
+  switchContainer.style.color = "black";
+};
+
+const switchTheme = (event) => {
+  if (event.target.checked) {
+    document.body.style.backgroundColor = "black";
+    darkMode();
+  } else {
+    document.body.style.backgroundColor = "white";
+    whiteMode();
+  }
+};
+
+toggleSwitch.addEventListener("change", switchTheme);
